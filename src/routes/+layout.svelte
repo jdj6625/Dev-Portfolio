60f3d27039fcc89d62b6836a90b427ca66968a56
Jonathan Jacobs <jonathanjacobs2113@gmail.com>
2026-02-19 10:06:09 -0500

    Init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import Header from "../components/Header.svelte";
+	import favicon from '$lib/assets/favicon.svg';
+	let { children } = $props();
+	import "../app.css"
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+<Header/>
+
+{@render children()}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,2 @@
+<h1>Welcome to SvelteKit</h1>
+<p class="text-cyan-300">Visit <a href="https://svelte.dev/docs/kit" >svelte.dev/docs/kit</a> to read the documentation</p>
